0783fe0e2b28b15352c4d6748f1ada543d95cca8
Anand-Prasad-14 <adrock4321anandgp@gmail.com>
2022-12-22 08:51:22 +0530

    first

---
+++ evenadd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int  main(){
+    int n;
+    cout << " enetr the number";
+    cin >> n ;
+    if(n%2 == 0){
+        cout << "the no. is divisible by 2";
+    }else{
+        cout << "the no. is  not diviible by 2";
+    }
+return 0;
+}
